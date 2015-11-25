dd043cdf076351ddf49f05e23add3ea6f8335a5e
Kim-Andersen <kimandersen1@gmail.com>
2015-11-25 16:42:44 +0100

    Improve StoryTimeline

--- app/components/StoryTimeline.js
+++ app/components/StoryTimeline.js
@@ -14,18 +14,27 @@ let StoryTimelineItem = React.createClass({
 		let className = 'story-timeline-item '+this.props.position
 		let mCreatedAt = moment(story.createdAt)
 		let uri = this.props.storyBaseUri+story._id
+		let text = story.text
+		let truncated = false
+
+		if(text.length > 200){
+			text = text.substring(0, 200) + '...'
+			truncated = true
+		}
 
 		return(
 			<div className={className}>
 				<Link to={uri} className="content">
 					<time dateTime={mCreatedAt.toISOString()}>{mCreatedAt.format('MMMM DD')}</time>
-					<p>{story.text}</p>
-					<div>
-					{story.hashtags && story.hashtags.map(function(hashtag, index){
-	      		return (<span href="#" key={index}>{hashtag} </span>)
-	      	})}
-					</div>
-					
+					<p>
+						{text}
+						{truncated ? <span className="anchor">Read more</span> : null}
+					</p>
+					<ul className="tag-list">
+						{story.hashtags && story.hashtags.map(function(hashtag, index){
+		      		return (<li key={index}>{hashtag} </li>)
+		      	})}
+					</ul>					
 				</Link>
 			</div>
 		)
